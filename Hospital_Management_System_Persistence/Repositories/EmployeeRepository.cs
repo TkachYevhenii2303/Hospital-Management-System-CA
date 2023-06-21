c0c36abd6146408531f95fc6124a0cdf62318fa1
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-06-21 17:15:13 +0300

    Finish for today

---
+++ Hospital_Management_System_Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using Hospital_Management_System_Applications.Interfaces.Patterns;
+using Hospital_Management_System_Applications.Interfaces.Repositories;
+using Hospital_Management_System_DAL.Entities;
+using Hospital_Management_System_DAL.Enums;
+using Hospital_Management_System_DAL.Wrapper_Response;
+using Hospital_Management_System_Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System_Persistence.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly IGenericRepository<Employees> _repository;
+        private readonly HospitalManagementContext _context;
+
+        public EmployeeRepository(IGenericRepository<Employees> repository, HospitalManagementContext context)
+        {
+            _repository = repository;
+            _context = context;
+        }
+
+        public async Task<Result<IEnumerable<Employees>>> ReturnEmployeeBySpecialization(string specialization)
+        {
+            var result = new Result<IEnumerable<Employees>>();
+
+            result.Entity = await _context.Set<Employees>()
+                .Include(x => x.HasRoles.Where(x => x.Roles.RolesTitle == specialization)).ToListAsync();
+
+            result.Message = $"Employee Retrieval by Specialization: " +
+                $"Congratulations on implementing the method {ReturnEmployeeBySpecialization(specialization).GetType().Name}";
+
+            return result;
+        }
+    }
+}
---
+++ Hospital_Management_System_Persistence/Repositories/Patterns/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using Hospital_Management_System_Applications.Interfaces.Patterns;
+using Hospital_Management_System_Domains.Common;
+using Hospital_Management_System_Persistence.Context;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System_Persistence.Repositories.Patterns
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly HospitalManagementContext _context;
+        private Hashtable _repositories;
+        private bool _disposed;
+
+        public UnitOfWork(HospitalManagementContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public void Dispose() => _context.Dispose();
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseAuditableEntity
+        {
+            if (_repositories is null) { _repositories = new Hashtable(); }
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
+                
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return _repositories[type] as IGenericRepository<TEntity>;
+
+        }
+
+        public Task Rollback()
+        {
+            _context.ChangeTracker.Entries().ToList().ForEach(x => x.Reload()); 
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> Save(CancellationToken cancellationToken)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
