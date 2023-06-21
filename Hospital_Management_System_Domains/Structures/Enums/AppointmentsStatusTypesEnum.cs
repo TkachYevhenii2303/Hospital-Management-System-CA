cc95137bc211503742431649d53c8fffccdb57b4
Tkach Yevhenii <109665913+TkachYevhenii2303@users.noreply.github.com>
2023-06-21 11:17:53 +0300

    adding new changes and new enums classes and wrapper for result

---
+++ Hospital_Management_System_Domains/Structures/Enums/AppointmentsStatusTypesEnum.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Hospital_Management_System_DAL.Enums
+{
+    public enum AppointmentsStatusTypesEnum
+    {
+        None = 0,
+
+        Started = 1,
+
+        Finished = 2, 
+
+        Deny = 3,
+
+        InProgress = 4,
+    }
+}
---
+++ Hospital_Management_System_Domains/Structures/Enums/RolesTypesEnum.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Hospital_Management_System_DAL.Enums
+{
+    public enum RolesTypesEnum
+    {
+        Dermatologist = 0,
+
+        Pediatrics = 1,
+
+        Neurologist  = 2,
+
+        Ophthalmology = 3,
+
+        Dermatology = 4,
+    }
+}
