2d70abbb0dedaca6db1e77e42c71d3d4174c4786
TkachYevhenii2303 <tkach.yevhenii23@gmail.com>
2023-08-14 16:32:14 +0300

    Refactoring the structure of folder and renaming the titles

---
+++ HospitalManagementSystem.Applications/Exceptions/Handling/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System_Applications.Exceptions.Handling
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public bool Success { get; set; }
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
