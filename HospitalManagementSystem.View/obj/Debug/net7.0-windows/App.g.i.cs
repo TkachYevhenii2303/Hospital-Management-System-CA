0bf8d1dd9ac17b882d7bdb8f55eec227bed50147
TkachYevhenii2303 <tkach.yevhenii23@gmail.com>
2023-10-25 10:17:11 +0300

    System files aterf updating project

--- HospitalManagementSystem.View/obj/Debug/net7.0-windows/App.g.i.cs
+++ HospitalManagementSystem.View/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace HospitalManagementSystem.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace HospitalManagementSystem.View {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             HospitalManagementSystem.View.App app = new HospitalManagementSystem.View.App();
             app.InitializeComponent();
